01c8a772cc52742b24e23fd640808c36d1039175
merobi-hub <merobinson@uri.edu>
2021-05-04 13:55:47 -0400

    first commit

---
+++ car_api/site/routes.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template
+
+site = Blueprint('site', __name__, template_folder='site_templates')
+
+"""
+In above code, some arguments are specified when creating the Blueprint object. This first
+argument 'site' is the blueprint's name, which will be used by flask's routing
+mechanism. The second parameter, __name__, is the blueprint's import name, which flask uses 
+to locate resources
+"""
+
+@site.route('/')
+def home():
+    return render_template('index.html')
+
+@site.route('/profile')
+def profile():
+    return render_template('profile.html')
---
+++ config.py
@@ -0,0 +1,14 @@
+import os #base dir, tells flask where to find project
+
+basedir = os.path.abspath(os.path.dirname(__file__)) #file refers to root dir in project tree
+
+# gives access to project in any operating sys and allows access to outside folders
+
+class Config():
+    """
+    Set config variables for the flask app here using env variables where available,
+    otherwise create them if not done already
+    """
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess...'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False #turns off notifications from db
